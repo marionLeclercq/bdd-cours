7795454f249e99ec7e1d32b501cbf08c0de345c4
Ricardo Tormo <ricardotormo.es@gmail.com>
2019-10-21 01:42:25 +0200

    add bootstrap ex 4

--- 2.SQL-1/Exercices/4.connexion.php
+++ 2.SQL-1/Exercices/4.connexion.php
@@ -7,6 +7,7 @@
   <title>Connexion avec PDO</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.css" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.5.0/styles/dracula.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <style>
     .container {
       max-width: 1000px;
