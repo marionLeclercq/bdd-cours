ecb2b16c08e75dca1c211037b41efc3fea5ea077
Ricardo Tormo <ricardotormo.es@gmail.com>
2019-10-23 05:28:43 +0200

    add interactive exos

---
+++ 3.SQL-2/Exercices/0.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title></title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.5.0/styles/dracula.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <style>
+    .container {
+      max-width: 1000px;
+      margin: 0 auto;
+    }
+    .hljs {
+      display: block !important;
+      overflow-x: auto !important;
+      padding: 0.5em !important;
+      background: #282a36 !important;
+    }
+  </style>
+</head>
+<body class="markdown-body">
+  <div class="container">
+  <br>
+    <b>Exercice: </b> <br>
+    Si vous voulez pratiquer SQL de façon interactive et vous entrainer,
+    vous pouvez tester ces sites:
+    <ul>
+      <li>
+        <a href="https://sqlbolt.com/lesson/select_queries_introduction">Sqlbolt</a>
+      </li>
+      <li>
+        <a href="https://dataschool.com/learn-sql">Dataschool</a>
+      </li>
+      <li>
+        <a href="https://www.sqlteaching.com">SqlTeaching</a>
+      </li>
+    </ul>
+    </ul>
+    <br>
+  </div>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</body>
+</html>
