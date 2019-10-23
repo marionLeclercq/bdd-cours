c6da7d3e070e9a92d7cef59ad24bdd1569d8beb2
Ricardo Tormo <ricardotormo.es@gmail.com>
2019-10-23 05:12:28 +0200

    SQL-2 exercices added

---
+++ 3.SQL-2/Exercices/4.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title></title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.5.0/styles/dracula.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <style>
+    .container {
+      max-width: 1000px;
+      margin: 0 auto;
+    }
+    .hljs {
+      display: block !important;
+      overflow-x: auto !important;
+      padding: 0.5em !important;
+      background: #282a36 !important;
+    }
+  </style>
+</head>
+<body class="markdown-body">
+  <div class="container">
+  <br>
+    <b>Exercice: </b> <br>
+    Rédigez une requête pour trier les 
+    clients de la table client avec toutes les 
+    informations dont l'id est
+     3007, 3008 et 3009. <br>
+    <pre>
+         id       |   nom          |    ville   |
+      ------------|----------------|------------|
+        3002      | Nicolas Sousa  | New York   |
+        3007      | Julie Davis    | New York   |
+        3005      | Franck Zusi    | California |
+        3008      | Julian Green   | London     |
+        3004      | Marta Sario    | Paris      |
+        3009      | Pepe Romero    | Berlin     |
+        3003      | Martin Fonte   | Moscow     |
+    </pre>
+    <br>
+  </div>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</body>
+</html>
---
+++ 3.SQL-2/Exercices/6.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title></title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.5.0/styles/dracula.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <style>
+    .container {
+      max-width: 1000px;
+      margin: 0 auto;
+    }
+    .hljs {
+      display: block !important;
+      overflow-x: auto !important;
+      padding: 0.5em !important;
+      background: #282a36 !important;
+    }
+  </style>
+</head>
+<body class="markdown-body">
+  <div class="container">
+  <br>
+    <b>Exercice: </b> <br>
+    Rédigez une instruction SQL pour trouver les 
+    vendeurs de la table vendeur.
+    <ul>
+      <li>
+      Le 1er 
+      caractère doit être un 'N', le 4ème 
+      caractère doit être 'l' et le reste peut être 
+      n'importe quel autre caractère.
+      </li>
+    </ul>
+    <br>
+    <pre>
+    vendeur_id    | nom_et_prenom |   ville     |   commission 
+    -----------------------------------------------------------
+        5001      | James Hoog    | Londres     |     0.15
+        5002      | Nail Knite    | Paris       |     0.13
+        5005      | Pit Alex      | Barcelone   |     0.11
+        5006      | Mc Lyon       | Paris       |     0.14
+        5007      | Paul Adam     | Madrid      |     0.13
+        5003      | Lauson Hen    | Paris       |     0.12
+    </pre>
+    <br>
+  </div>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</body>
+</html>
