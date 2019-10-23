d6cfb17380159788f0d86d368dabc0fc6aacc1c8
Ricardo Tormo <ricardotormo.es@gmail.com>
2019-10-23 15:42:25 +0200

    add 2 first solutions SQL3

---
+++ 3.SQL-2/Solutions/1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title></title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.5.0/styles/dracula.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <style>
+    .container {
+      max-width: 1000px;
+      margin: 0 auto;
+    }
+    .hljs {
+      display: block !important;
+      overflow-x: auto !important;
+      padding: 0.5em !important;
+      background: #282a36 !important;
+    }
+  </style>
+</head>
+<body class="markdown-body">
+  <div class="container">
+  <br>
+    <b>Exercice: </b> <br>
+    Dans la table country de la base de données world, 
+    sélectionnez le continent et afficher la population totale par
+    continent.
+    </ul>
+    <br>
+    <b>Solution:</b>
+    <pre>
+      SELECT continent, SUM(population) FROM country GROUP BY continent;
+    </pre>
+    <br>
+  </div>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</body>
+</html>
---
+++ 3.SQL-2/Solutions/2.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title></title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.5.0/styles/dracula.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <style>
+    .container {
+      max-width: 1000px;
+      margin: 0 auto;
+    }
+    .hljs {
+      display: block !important;
+      overflow-x: auto !important;
+      padding: 0.5em !important;
+      background: #282a36 !important;
+    }
+  </style>
+</head>
+<body class="markdown-body">
+  <div class="container">
+  <br>
+    <b>Exercice: </b> <br>
+    Afficher combien de langues officiels existent en Afganistan.
+    Pour ce faire, utilisez la table countrylanguage de la base 
+    de données world. <br>
+    Une piste, 
+    <a href="https://sql.sh/cours/alias">
+    vous pouvez 
+    utiliser un alias pour renommer la colonne avec le nom langues
+    </a>
+    Le résultat doit apparaître sous cette forme:
+    <br>
+    <table>
+        <tbody>
+          <tr>
+            <td><b>countryCode</b></td>
+            <td><b>langues</b></td>
+          </tr>
+          <tr>
+            <td> AFG </td>
+            <td> XXX </td>
+          </tr>
+        </tbody>
+      </table>
+    </ul>
+    <br>
+    <b>Solution: </b>
+    <pre>
+      SELECT countryCode, COUNT(*) as langues FROM countrylanguage where CountryCode = "AFG" AND IsOfficial = "T";
+    </pre>
+  </div>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</body>
+</html>
